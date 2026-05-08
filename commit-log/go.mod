ff3326361ab96c8eb7b4acae0e08c03ae7f9cd4a
saiharsha <harshanaidu3330@gmail.com>
2026-05-08 22:40:11 +0530

    commit log

--- commit-log/go.mod
+++ commit-log/go.mod
@@ -1,3 +1,11 @@
 module github.com/harsha3330/ds-with-go/commit-log
 
 go 1.25.1
+
+require (
+	github.com/gogo/protobuf v1.3.2
+	github.com/harsha3330/ds-with-go/proglog v0.0.0-20260508170142-ae9ecb841f60
+	github.com/tysonmote/gommap v0.0.3
+)
+
+require google.golang.org/protobuf v1.36.11 // indirect
--- commit-log/internal/log/segment.go
+++ commit-log/internal/log/segment.go
@@ -6,7 +6,7 @@ import (
 	"path"
 
 	"github.com/gogo/protobuf/proto"
-	api "github.com/harsha3330/proglog/api/v1"
+	api "github.com/harsha3330/ds-with-go/proglog/api/v1"
 )
 
 type segment struct {
@@ -87,4 +87,39 @@ func (s *segment) Read(off uint64) (*api.Record, error) {
 	record := &api.Record{}
 	err = proto.Unmarshal(p, record)
 	return record, err
+}
+
+func (s *segment) Close() error {
+	if err := s.index.Close(); err != nil {
+		return err
+	}
+	if err := s.store.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func nearestMultiple(j, k uint64) uint64 {
+	if j >= 0 {
+		return (j / k) * k
+	}
+	return ((j - k + 1) / k) * k
+}
+
+func (s *segment) IsMaxed() bool {
+	return s.store.size >= s.config.Segment.MaxStoreBytes ||
+		s.index.size >= s.config.Segment.MaxIndexBytes
+}
+
+func (s *segment) Remove() error {
+	if err := s.Close(); err != nil {
+		return err
+	}
+	if err := os.Remove(s.index.Name()); err != nil {
+		return err
+	}
+	if err := os.Remove(s.store.Name()); err != nil {
+		return err
+	}
+	return nil
 }
